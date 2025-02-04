1bc6e07236f812be39867eb6ee3c2f6314114f80
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2025-02-04 10:35:41 +0700

    config: update interceptor response data

--- src/utils/axios.customize.js
+++ src/utils/axios.customize.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+	baseURL: import.meta.env.VITE_BACKEND_URL,
 });
-
 
 // Important: If axios is used with multiple domains, the AUTH_TOKEN will be sent to all of them.
 // See below for an example using Custom instance defaults instead.
 
 instance.defaults.headers.post["Content-Type"] =
 	"application/x-www-form-urlencoded";
-
 
 // Add a request interceptor
 instance.interceptors.request.use(
@@ -29,6 +27,7 @@ instance.interceptors.response.use(
 	function (response) {
 		// Any status code that lie within the range of 2xx cause this function to trigger
 		// Do something with response data
+		if (response && response.data) return response.data;
 		return response;
 	},
 	function (error) {
